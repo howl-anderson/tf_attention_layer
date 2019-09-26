699936d1e13966d5ddf23338226c1aad1157b503
Xiaoquan Kong <u1mail2me@gmail.com>
2019-09-26 17:15:55 +0800

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tf_attention_layer",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(include=["tf_attention_layer", "tf_attention_layer.*"]),
     url="https://github.com/howl-anderson/tf_attention_layer",
     license="MIT",
